b414f6de8355caf58906b4fcf5f2342334e7ac87
wangonya <kwangonya@gmail.com>
2020-04-04 22:35:55 +0300

    add setup file

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="cchat",
+    version="0.1.0",
+    py_modules=find_packages(),
+    description="Cli chat app made with Python & Twilio's Programmable Chat "
+                "REST API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wangonya/cchat",
+    author="Kinyanjui Wangonya",
+    author_email="kwangonya@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    install_requires=[
+        "Click",
+        "requests",
+    ],
+    entry_points="""
+        [console_scripts]
+        cchat=app:cli
+        """,
+)
