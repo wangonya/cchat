7cb0a14c88c0bafd657b37390f94fe8fb4f37a07
wangonya <kwangonya@gmail.com>
2020-04-06 07:13:10 +0300

    add new users, add them to general channel, create general channel if it doesn't exist

--- app/__init__.py
+++ app/__init__.py
@@ -42,18 +42,50 @@ def cli(ctx):
 @click.argument('identity')
 @click.pass_obj
 def login(ctx, identity):
+    """Creates a new user with the given `identity`.
+    The user `identity` and `sid` are written in a `.cchat.cfg` file for
+    reference.
+    Each new user automatically becomes a member of the general channel."""
     try:
+        # create new user
         user = client.chat.services(ctx.service_sid).users.create(
             identity=identity)
         config['user'] = {}
         config['user']['identity'] = identity
         config['user']['sid'] = user.sid
+
+        # save user details in .cchat.cfg
         with open('.cchat.cfg', 'w+') as configfile:
             config.write(configfile)
-        spinner.info(f"New user created: {identity}.")
+        spinner.info(f"New user created: {identity}")
+
+        # add user to general channel
+        try:
+            channel = client.chat.services(ctx.service_sid).channels(
+                'general').fetch()
+            client.chat.services(ctx.service_sid).channels(
+                channel.sid).members.create(identity=identity)
+            spinner.info(f"{identity} added to {channel.unique_name}")
+        except TwilioRestException as err:
+            if err.status == 404:
+                # if general channel doesn't exist, create it and add the user
+                spinner.info("Creating general channel...")
+                channel = client.chat.services(ctx.service_sid) \
+                    .channels.create(
+                    friendly_name='General Channel',
+                    unique_name='general',
+                    created_by=identity
+                )
+                spinner.info(f"Channel '{channel.unique_name}' created")
+                client.chat.services(ctx.service_sid).channels(
+                    channel.sid).members.create(identity=identity)
+                spinner.info(f"{identity} added to {channel.unique_name}")
+            else:
+                spinner.fail(err.msg)
+
     except TwilioRestException as err:
-        if err.status == 409:
-            spinner.info(f"Welcome back, {identity}.")
+        if err.status == 409:  # user exists
+            spinner.info(f"Welcome back, {identity}")
         else:
             spinner.fail(err.msg)
 
